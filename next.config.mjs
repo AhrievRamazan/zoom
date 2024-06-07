b3b787df5dfd321d1a80f931ec61d705aa0ead18
AhrievRamazan <ramazanahriev278@gmail.com>
2024-06-08 00:59:27 +0300

    zoom7

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    env: {
+        CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+      },
 };
 
 export default nextConfig;
