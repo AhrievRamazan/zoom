fe0c30b022445aa55be8e8cc157d4c24d07deee1
AhrievRamazan <ramazanahriev278@gmail.com>
2024-06-08 00:01:04 +0300

    zoom4

--- .next/server/pages/index.js
+++ .next/server/pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
       <div>
-        <h1>Welcome to Next.js!</h1>
+        <h1>Welcome to Next.js on Vercel!</h1>
         <p>This is the home page.</p>
       </div>
     );
